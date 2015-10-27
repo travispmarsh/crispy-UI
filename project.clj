28a6cb7e6b8e0c844a7331ffe216d4d73dcc67c0
Stephen Starkey <scstarkey@gmail.com>
2015-10-27 06:55:34 -0500

    License.

--- project.clj
+++ project.clj
@@ -1,3 +1,22 @@
+;
+; Copyright (c) 2015, Courage Labs, LLC.
+;
+; This file is part of Crispy Tatertot.
+;
+; Crispy Tatertot is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Affero General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; Crispy Tatertot is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU Affero General Public License for more details.
+;
+; You should have received a copy of the GNU Affero General Public License
+; along with Crispy Tatertot.  If not, see <http://www.gnu.org/licenses/>.
+;
+
 (defproject crispy-tatertot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
