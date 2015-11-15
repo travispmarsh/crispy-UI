a447e0442dcc8ea3942be3c7bc22a46b6e30347a
Stephen Starkey <scstarkey@gmail.com>
2015-11-15 07:36:13 -0600

    Didn't realize that I got it all backwards.

--- src/crispy_tatertot/handler.clj
+++ src/crispy_tatertot/handler.clj
@@ -1,20 +1,20 @@
 ;
 ; Copyright (c) 2015, Courage Labs, LLC.
 ;
-; This file is part of Crispy Tatertots.
+; This file is part of Crispy Tatertot.
 ;
-; Crispy Tatertots is free software: you can redistribute it and/or modify
+; Crispy Tatertot is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU Affero General Public License as published by
 ; the Free Software Foundation, either version 3 of the License, or
 ; (at your option) any later version.
 ;
-; Crispy Tatertots is distributed in the hope that it will be useful,
+; Crispy Tatertot is distributed in the hope that it will be useful,
 ; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ; GNU Affero General Public License for more details.
 ;
 ; You should have received a copy of the GNU Affero General Public License
-; along with Crispy Tatertots.  If not, see <http://www.gnu.org/licenses/>.
+; along with Crispy Tatertot.  If not, see <http://www.gnu.org/licenses/>.
 ;
 
 (ns crispy-tatertot.handler
