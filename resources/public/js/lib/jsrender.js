1089e3112d739ceb43fbe313996057c95db72bc1
Stephen Starkey <scstarkey@gmail.com>
2015-11-15 07:19:43 -0600

    Removed accidental copyright headers.

--- resources/public/js/lib/jsrender.js
+++ resources/public/js/lib/jsrender.js
@@ -1,24 +1,14 @@
-/*
- * Copyright (c) 2015, Courage Labs, LLC.
- *
- * This file is part of Crispy Tatertots.
- *
- * Crispy Tatertots is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Crispy Tatertots is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Crispy Tatertots.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 /*! JsRender v1.0.0-rc.65 (Beta - Release Candidate): http://jsviews.com/#jsrender */
 /*! **VERSION FOR WEB** (For NODE.JS see http://jsviews.com/download/jsrender-node.js) */
+/*
+ * Best-of-breed templating in browser or on Node.js.
+ * Does not require jQuery, or HTML DOM
+ * Integrates with JsViews (http://jsviews.com/#jsviews)
+ *
+ * Copyright 2015, Boris Moore
+ * Released under the MIT License.
+ */
+
 //jshint -W018, -W041
 
 (function(factory) {
